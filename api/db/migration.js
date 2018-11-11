0fc8deed59ab37d835c4997adbc5db3cab39934e
Daniel Chen <chen.daniel@husky.neu.edu>
2018-11-11 01:37:50 -0500

    Add new migrations

--- api/db/migration.js
+++ api/db/migration.js
@@ -4,11 +4,17 @@ const dao = new AppDAO('./db/app.db');
 
 const Accounts = require('../src/models/accounts');
 const SessionTokens = require('../src/models/sessionTokens');
+const Groups = require('../src/models/groups');
+const UsersToGroups = require('../src/models/usersToGroups');
 
-const accounts = new Accounts(dao);
-const sessionTokens = new SessionTokens(dao);
+const accounts = new Accounts();
+const sessionTokens = new SessionTokens();
+const groups = new Groups();
+const usersToGroups = new UsersToGroups();
 
 accounts.createTable();
 sessionTokens.createTable();
+groups.createTable();
+usersToGroups.createTable();
 
 dao.close();
