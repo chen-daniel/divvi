b0b67365adc5e481f92e7467e0d3f91e44fc6967
Abel Shin <ashin@basistech.com>
2018-11-11 00:36:18 -0500

    Added users and groups interaction

--- api/src/routes/api/v1.js
+++ api/src/routes/api/v1.js
@@ -14,6 +14,31 @@ router
   .delete(v1Controller.sessions.deleteSession);
 
 router
+  .route('/groups')
+  .get(v1Controller.groups.getAllGroups);
+  
+router
+  .route('/groups/:groupsId')
+  .get(v1Controller.groups.getGroup)
+  .post(v1Controller.groups.createSession)
+  .delete(v1Controller.groups.deleteSession);
+
+router
+  .route('/userGroups')
+  .get(v1Controller.userToGroups.getUserGroup)
+  .post(v1Controller.userToGroups.createUserToGroup)
+
+router
+  .route('/userGroups/:userGroupId')
+  .delete(v1Controller.userToGroups.deleteUserGroup);
+  
+router
+  .route('/groups/:groupsId')
+  .get(v1Controller.groups.getGroup)
+  .post(v1Controller.groups.createSession)
+  .delete(v1Controller.groups.deleteSession);
+
+router
   .route('/accounts')
   .get(v1Controller.accounts.getAllAccounts);
 
